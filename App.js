4a19e4238840566b6ff6ae28f867588deeba1831
miriamela <grossi.miriam@gmail.com>
2021-04-13 19:26:05 +0200

    fetched data from database

--- App.js
+++ App.js
@@ -3,20 +3,18 @@ import React from 'react';
 // import { StyleSheet, View } from 'react-native';
 import Main from "./src/components/Main";
 import {NativeRouter} from "react-router-native";
+import {ApolloProvider} from "@apollo/client";
+import createApolloClient from "./src/utils/apolloClient";
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const apolloClient =createApolloClient();
+
 
 function App() {
   return (
     <NativeRouter>
+      <ApolloProvider client={apolloClient}>
         <Main/>
+      </ApolloProvider>   
     </NativeRouter>
   );
 }
